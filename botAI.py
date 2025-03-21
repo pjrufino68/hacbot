0cbe0b1f819dbe00d803447db37c56b1e35430ef
pjrufino68 <pjfernandesrufino@gmail.com>
2025-03-21 09:38:50 -0300

    Add files via upload

--- botAI.py
+++ botAI.py
@@ -1,8 +1,10 @@
 import streamlit as st
+import speech_recognition as sr
 import openai
 import keyboard
 import os
 import psutil
+import time
 from dotenv import load_dotenv
 
 from openai import OpenAI
@@ -42,22 +44,52 @@ st.session_state["messages"] = lista
 
 @st.cache_data
 
-def enviarIA(textoRecebido, lista):
+def enviarIA(textoRecebido, _lista):
     lista.append({"role": "user", "content": texto})
     response = client.chat.completions.create(model="gpt-4o-mini", messages=lista)
     lista.append(response)
     return response.choices[0].message.content
 
+def reconhecer_voz():
+    # Inicializa o reconhecedor de voz
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        st.write("Por favor, fale agora...")
+        audio = r.listen(source)
+    
+    try:
+        # Converte áudio em texto
+        texto = r.recognize_google(audio, language="pt-BR")
+        st.write(f"Você : {texto}")
+        return texto
+    except sr.UnknownValueError:
+        st.write("Não entendi o áudio.")
+        return ""
+    except sr.RequestError:
+        st.write("Erro na solicitação ao serviço de reconhecimento de voz.")
+        return ""
+    
 with st.container():
     i = 0
     texto = ""
+    texto_voz = ""
 
     while True:
-        texto = st.text_input("Faça sua pergunta: ", key=i)
+        forma = st.radio("", ["Escrever", "Falar :studio_microphone:"], 
+            horizontal=True, key=f'{i}'
+        )
+
+        if forma == "Escrever":
+            texto = st.text_input("", placeholder="Faça sua pergunta", key=f'texto_{i}')
+        else:
+            texto_voz = reconhecer_voz()
+            texto = texto_voz
+
         if texto == "fim" or len(texto) < 1:
             break
         else:
             msg = enviarIA(texto, lista)
             st.chat_message("assistant").write(msg)
         i = i + 1
+        
 
